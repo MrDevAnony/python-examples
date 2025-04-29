c5fade9b5e6dde174ae29916d5829f6954bfc85e
MrDevAnony <mrdevanony@gmail.com>
2025-04-29 17:05:07 +0330

    Update

--- for.py
+++ for.py
@@ -1,7 +1,7 @@
 list_ = [5, 14, 28, 30, 26, 45]
 
 for i in list(filter(lambda x:x>29, list_)):
-    print( i, " is Bigger than 29: ")
+    print( i, " is Bigger than 29.")
         
 for i in list(filter(lambda x:x<29, list_)):
-    print(i, " is smaller  than 29")
+    print(i, " is smaller  than 29.")
