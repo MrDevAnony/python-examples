6adaba3a648313970e4358f3f52076b507c36c26
DevAnony <143434705+MrDevAnony@users.noreply.github.com>
2025-05-05 10:51:38 +0330

    Update reverse_name.py

--- reverse_name.py
+++ reverse_name.py
@@ -1,14 +1,14 @@
 name = input("enter your name: ")
 
 # First way
-for i in range(len(s)-1, -1, -1):
-    print(s[i], end="")
+for i in range(len(name)-1, -1, -1):
+    print(name[i], end="")
+print()
 # Second way
 print(name[::-1])
-# My way
-index = len(name) - 1
+# My way                                      index = len(name) - 1
 reversed_name = ""
 while 0 <= index:
-    reversed_name = reversed_name + name[index]
+    reversed_name = reversed_name + name[inde>
     index = index - 1
 print(reversed_name)
