aba530f41c26a1be2301074a5268ddfb6f30f9e2
DevAnony <143434705+MrDevAnony@users.noreply.github.com>
2025-05-05 10:31:29 +0330

    Update reverse_name.py

--- reverse_name.py
+++ reverse_name.py
@@ -5,3 +5,10 @@ for i in range(len(s)-1, -1, -1):
     print(s[i], end="")
 # Second way
 print(name[::-1])
+# My way
+index = len(name) - 1
+reversed_name = ""
+while 0 <= index:
+    reversed_name = reversed_name + name[index]
+    index = index - 1
+print(reversed_name)
